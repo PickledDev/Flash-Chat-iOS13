3c78dd1f4766fbcf7eeb66515b84b1d82032cab9
Michael Irwin <33363962+PickledDev@users.noreply.github.com>
2020-11-14 10:39:05 -0600

    Simple chat application
    Learning iOS made a simple chat app

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Michael Irwin on 2020/21/01.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+  let sender: String  //contains email of sender
+  let body: String //this is the actual message
+}
